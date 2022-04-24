0ede902a24670a493146f64ee10ec4eb3df7df7d
Aditya Chari S <84094715+aditya-chari25@users.noreply.github.com>
2022-04-24 14:53:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,24 @@ import requests
 import json
 
 html_text = requests.get('https://www.espncricinfo.com/live-cricket-match-results').text
+live_text = requests.get('https://www.espncricinfo.com/live-cricket-score').text
+all_text = requests.get('https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/match-results').text
 
 # print(html_text)
 soup = BeautifulSoup(html_text,'lxml')                  
 jobs = soup.find_all('div',class_="ds-px-4 ds-py-3");
 
+soup_live = BeautifulSoup(live_text,'lxml')
+live_job = soup_live.find_all('div',class_="ds-px-4 ds-py-3");
+
+soup_all = BeautifulSoup(all_text,'lxml')
+all_job = soup_all.find_all('div',class_="ds-px-4 ds-py-3");
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<h2>Welcome to the IPL API</h2>"
 
 @app.route("/fixture-results")
 def score_results():
@@ -51,6 +58,81 @@ def score_results():
                 "team2_score":score_2,
             })
     return {"IPL_data":arr_score}
+
+@app.route("/live-score")
+def live_results():
+    arr_score = []
+    a=15
+    for score in live_job:
+        descp = score.find('div',class_='ds-text-tight-xs ds-truncate ds-text-ui-typo-mid')
+        scoring_team = score.find_all('div',class_="ds-text-compact-s ds-text-typo-title")
+        if(descp.a.span.text=='Indian Premier League'and(len(scoring_team))):
+            scores_team = score.find_all('div',class_="ds-text-compact-s ds-text-typo-title")
+            print(scores_team)
+            score_1 = scoring_team[0].strong.text
+            print(score_1)
+            score_2 = scoring_team[1].strong.text
+            link = score.find('a',class_='').get('href')
+            twoteams = score.find_all('p',class_='ds-text-tight-m ds-font-bold ds-capitalize')
+            winner = score.find('p',class_='ds-text-tight-s ds-font-regular ds-truncate ds-text-typo-title').span.text
+            team_1 = twoteams[0]
+            team_2 = twoteams[1]
+            match_date = descp.text.split(',')[0]
+            location = descp.text.split(',')[1]
+
+            info_scores={
+                "Match Type" : descp.a.span.text,
+                "Match_Result": winner,
+                "team1_score":score_1,
+                "team2_score":score_2
+            }
+            arr_score.append({
+                "Match Type" : descp.a.span.text,
+                "Match_Date" : match_date,
+                "Match_Location" : location,
+                "Match_Result": winner,
+                "team_1":team_1.text,
+                "team_2":team_2.text,
+                "team1_score":score_1,
+                "team2_score":score_2,
+            })
+    return {"Live_IPL_data":arr_score}
+
+@app.route("/all-match-results")
+def allmatch_results():
+    arr_score = []
+    a=15
+    for score in all_job:
+        descp = score.find('div',class_='ds-text-tight-xs ds-truncate ds-text-ui-typo-mid')
+        if(descp.a.span.text=='Indian Premier League'):
+            scores_team = score.find_all('div',class_="ds-text-compact-s ds-text-typo-title")
+            score_1 = scores_team[0].strong.text
+            score_2 = scores_team[1].strong.text
+            link = score.find('a',class_='').get('href')
+            twoteams = score.find_all('p',class_='ds-text-tight-m ds-font-bold ds-capitalize')
+            winner = score.find('p',class_='ds-text-tight-s ds-font-regular ds-truncate ds-text-typo-title').span.text
+            team_1 = twoteams[0]
+            team_2 = twoteams[1]
+            match_date = descp.text.split(',')[0]
+            location = descp.text.split(',')[1]
+
+            info_scores={
+                "Match Type" : descp.a.span.text,
+                "Match_Result": winner,
+                "team1_score":score_1,
+                "team2_score":score_2
+            }
+            arr_score.append({
+                "Match Type" : descp.a.span.text,
+                "Match_Date" : match_date,
+                "Match_Location" : location,
+                "Match_Result": winner,
+                "team_1":team_1.text,
+                "team_2":team_2.text,
+                "team1_score":score_1,
+                "team2_score":score_2,
+            })
+    return {"Match-Results":arr_score}
     
             
 
